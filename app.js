320aa046a4dc09bdbd2c8af2b9e5bc91031b1f67
Artpunk <ozerland@gmail.com>
2021-06-28 00:20:20 +0300

    Add logger middlware

--- app.js
+++ app.js
@@ -1,55 +1,26 @@
 const express = require('express');
-const path = require('path');
-
-const {data} = require('./data')
-
 const app = express()
+const logger = require('./middleware/logger')
 
-// setup static and middleware
-app.use(express.static('./front-app'))
+// req => middleware => res
 
-app.get('/data', (req, res) => {
-    const newData = data.map((item) => {
-        const {id, desc, img} = item;
-        return {id, desc, img}
-    });
-    res.json(newData)
-})
+app.use('/api',logger)
 
-app.get('/data/:dataID', (req, res) => {
-    const {dataID} = req.params;
-    const singleData = data.find((item) => item.id === Number(dataID))
+app.get('/', (req, res) => {
+    res.send('Home')
+});
 
-    if (!singleData) {
-      return res.status(404).send('<h1>404 NOT FOUND</h1>')
-    }
-    return res.json(singleData)
-})
+app.get('/about',  (req, res) => {
+    res.send('About')
+});
 
-app.get('/data/v1/query', (req, res) => {
-    const {search, limit} = req.query
-    let sortedArray = [...data]
+app.get('/products',  (req, res) => {
+    res.send('Products')
+});
 
-    if (search) {
-        sortedArray = sortedArray.filter((item) => {
-            return item.desc.startsWith(search)
-        })
-    }
-
-    if (limit) {
-        sortedArray = sortedArray.slice(0, Number(limit))
-    }
-
-    if (sortedArray.length < 1) {
-       return  res.status(200).json({success: true, data: [{name: 'Artpunk'}]})
-    }
-
-   return res.status(200).json(sortedArray)
-})
-
-app.all('*', (req, res) => {
-    res.status(404).send('<h1>404 NOT FOUND</h1>')
-})
+app.get('/items',  (req, res) => {
+    res.send('Items')
+});
 
 app.listen(5000, () => {
     console.log('server is listening on port 5000...')
